b9b3080aa4bda08582955cf40bf6610d8398b081
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-06-16 21:49:38 -0300

    improve: Search system

--- src/js/search.js
+++ src/js/search.js
@@ -3,31 +3,59 @@ const btnSubmit = document.querySelector('#btnSubmit')
 
 btnSubmit.addEventListener('click', () => {
 	var searchTerm = inputSearch.value.toLowerCase()
+	searchTerm = searchTerm.replace('ã', 'a')
+	searchTerm = searchTerm.replace('õ', 'o')
+	searchTerm = searchTerm.replace('á', 'a')
+	searchTerm = searchTerm.replace('ó', 'o')
+	searchTerm = searchTerm.replace('é', 'e')
+	searchTerm = searchTerm.replace('í', 'i')
+	searchTerm = searchTerm.replace('ô', 'o')
+	searchTerm = searchTerm.replace('á', 'a')
 
+	console.log(searchTerm)
 	search(searchTerm)
 })
 
 function search(searchTerm) {
+	const initial = '#start'
+	const initialSection = '#infoFamily'
+	const foodSection = '#food'
+	const questionsSection = '#questions'
+	const infoSection = '#info'
+
 	if (
-		searchTerm === 'família' ||
+		searchTerm === 'familia' ||
 		searchTerm === 'famila' ||
-		searchTerm === 'famílha' ||
 		searchTerm === 'familha' ||
 		searchTerm === 'family' ||
 		searchTerm === 'mozzini' ||
 		searchTerm === 'muccini' ||
 		searchTerm === 'mossini'
 	) {
-		window.location.href = '#infoFamily'
-	} else if (searchTerm === 'pratos') {
-		window.location.href = '#food'
+		window.location.href = initialSection
+		//
+	} else if (
+		searchTerm === 'prato' ||
+		searchTerm === 'pratos' ||
+		searchTerm === 'comida' ||
+		searchTerm === 'comidas'
+	) {
+		window.location.href = foodSection
+		//
 	} else if (searchTerm === 'restaurante') {
-		window.location.href = '#info'
+		window.location.href = infoSection
+		//
 	} else if (searchTerm === 'dúvidas') {
-		window.location.href = '#questions'
+		window.location.href = questionsSection
+		//
 	} else if (searchTerm === 'horário') {
-		window.location.href = '#info'
+		window.location.href = infoSection
+		//
+	} else if (searchTerm === 'romulo' || searchTerm === 'virginia') {
+		window.location.href = infoSection
+		//
 	} else {
-		window.location.href = '#start'
+		window.location.href = initial
+		//
 	}
 }
