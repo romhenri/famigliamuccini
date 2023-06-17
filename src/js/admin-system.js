2eed34a3917600127b747e4fb7bf9c919b64bf3e
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-06-16 22:27:28 -0300

    Versão 3.0

--- src/js/admin-system.js
+++ src/js/admin-system.js
@@ -3,7 +3,12 @@ const btnAdmin = document.querySelector('.btnAdmin')
 btnAdmin.addEventListener('click', () => {
 	const triedWord = prompt('Senha de Acesso:')
 
-	if (triedWord == 'mamma' || triedWord == 'mozzini') {
+	if (
+		triedWord == 'mamma' ||
+		triedWord == 'mozzini' ||
+		triedWord == 'napoli' ||
+		triedWord == 'milani'
+	) {
 		window.location.href =
 			'https://romhenri.github.io/javascript/muccini-admin/'
 	} else {
